b6f939c0f2034c4f944353660065524578b2f86c
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-14 09:52:13 -0500

    Finds files and sub folder files

--- AutoConvert/src/main/java/Pandoc/Main.java
+++ AutoConvert/src/main/java/Pandoc/Main.java
@@ -1,9 +1,12 @@
 package Pandoc;
 
+import Pandoc.Native.Operations;
+
 public class Main {
     public static void main(String[] args){
         Display.checkPandocPresent();
         Display.displayMenu();
         Input.getMenuSelection(1,3);
+        Operations.findFiles();
     }
 }
--- AutoConvert/src/main/java/Pandoc/Native/Operations.java
+++ AutoConvert/src/main/java/Pandoc/Native/Operations.java
@@ -1,8 +1,7 @@
 package Pandoc.Native;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Operations {
     public static boolean checkForPandoc() {
@@ -19,5 +18,24 @@ public class Operations {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public static void findFiles(){
+        ArrayList<File> files = new ArrayList<File>();
+        listf(System.getProperty("user.dir"),files);
+    }
+
+    public static void listf(String directoryName, ArrayList<File> files) {
+        File directory = new File(directoryName);
+
+        // get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList) {
+            if (file.isFile() && file.getName().endsWith(".docx")) {
+                files.add(file);
+            } else if (file.isDirectory()) {
+                listf(file.getAbsolutePath(), files);
+            }
+        }
     }
 }
