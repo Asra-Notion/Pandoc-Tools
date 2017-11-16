2686e55eb6d74a6a3ddba504160c7c8cc7669536
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-16 09:32:55 -0500

    Finalize version 1

--- AutoConvert/src/main/java/Pandoc/Display.java
+++ AutoConvert/src/main/java/Pandoc/Display.java
@@ -1,24 +1,22 @@
 package Pandoc;
 
 import Pandoc.Native.Operations;
-import javafx.application.Application;
 
 public class Display {
 
-    public static void displayWelcome(){
+    public static void displayWelcome() {
         System.out.println("Welcome to Pandoc-Tools AutoConvert");
     }
 
-    public static void displayMenu(){
+    public static void displayMenu() {
         System.out.println("Select an input format:");
         System.out.println("1. .md");
         System.out.println("2. .docx");
         System.out.println("3. .pdf");
     }
 
-    public static void checkPandocPresent(){
-        if (!Operations.checkForPandoc())
-        {
+    public static void checkPandocPresent() {
+        if (!Operations.checkForPandoc()) {
             System.out.println("Pandoc not found in the path value. Please install pandoc to use this tool.");
             try {
                 Thread.sleep(5000);
@@ -41,7 +39,7 @@ public class Display {
         System.out.println("4. .html");
     }
 
-    public static void errorNoFilesFound(){
+    public static void errorNoFilesFound() {
         System.out.println("Error, no files found of specified type. Exitting.");
         try {
             Thread.sleep(5000);
@@ -49,5 +47,10 @@ public class Display {
             e.printStackTrace();
         }
         System.exit(1);
+    }
+
+    public static void convertingFileFromTo(String s, String s1) {
+        System.out.println("Converting file :" + s);
+        System.out.println("To :" + s1);
     }
 }
