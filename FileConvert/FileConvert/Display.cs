265ddcd5cb6bd1e0e69e1848704ef54102f11b6b
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-18 15:37:37 -0500

    AutoConvert init C# app

---
+++ FileConvert/FileConvert/Display.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConvert
+{
+    static class Display
+    {
+        public static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to Pandoc-Tools AutoConvert");
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("Select an input format:");
+            Console.WriteLine("1. .md");
+            Console.WriteLine("2. .docx");
+            Console.WriteLine("3. .pdf");
+        }
+    }
+}
---
+++ FileConvert/FileConvert/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display.DisplayWelcome();
+            Console.ReadLine();
+            //TODO: Wait with a timer
+        }
+    }
+}
