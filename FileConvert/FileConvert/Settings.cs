66cb958d76f682cfd231b98d630c0e71ae0484f8
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-20 17:29:40 -0500

    Init app settings

---
+++ FileConvert/FileConvert/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConvert
+{
+    public class Settings
+    {
+        public string InputFormat { get; set; }
+        public string OutputFormat { get; set; }
+        public string WorkingFolder { get; set; }
+        public bool UseWorkingFolder { get; set; }
+        public string OutputFolder { get; set; }
+
+        /// <summary>
+        /// Initialise the application default settings
+        /// </summary>
+        public Settings()
+        {
+
+        }
+
+        /// <summary>
+        /// Init the app with the user saved settings
+        /// </summary>
+        /// <param name="useUserSettings">true to use the settings, false to use the program arguments</param>
+        /// <param name="args">Launch arguments</param>
+        public Settings(bool useUserSettings, string[] args)
+        {
+
+        }
+    }
+}
