b8c734931945d5b0041cd9fe47c8b08b9daad22b
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-20 21:54:49 -0500

    Java conversion

--- FileConvert/FileConvert/PandocFile.cs
+++ FileConvert/FileConvert/PandocFile.cs
@@ -8,7 +8,50 @@ namespace FileConvert
 {
     public class PandocFile
     {
-        private string InputPath;
-        private string OutputPath;
+        public string Extension { get; private set; }
+        public string FileNameAndPath { get; private set; }
+
+        public PandocFile(string completePath, string type)
+        {
+            int index = completePath.LastIndexOf("type");
+            FileNameAndPath = completePath.Substring(0, index);
+            Extension = completePath.Substring(index);
+        }
+
+        public PandocFile(PandocFile copy)
+        {
+            this.Extension = copy.Extension;
+            this.FileNameAndPath = copy.FileNameAndPath;
+        }
+
+        public void ChangeFileExtension(string newExtension)
+        {
+            this.Extension = newExtension;
+        }
+
+        public String provideCompletePath()
+        {
+            String tmp = FileNameAndPath + Extension;
+            return tmp;
+        }
+
+        public void modifyFilePathRelative(String newFolder, String currentDirectory)
+        {
+            String filename = FileNameAndPath.Substring(currentDirectory.Length + 1);
+            if (!newFolder.EndsWith("\\"))
+            {
+                newFolder += '\\';
+            }
+            FileNameAndPath = newFolder + filename;
+            //System.out.println("filename:" + filename);
+            //System.out.println("cwd:" + currentDirectory);
+            //System.out.println("result:" + filenameAndPath);
+        }
+
+        public String getFolder()
+        {
+            String folder = FileNameAndPath.Substring(0, FileNameAndPath.LastIndexOf("\\"));
+            return folder;
+        }
     }
 }
