8e8c48c2391b6649d5b714a5703d80dde7a2feb2
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-21 08:59:01 -0500

    fix build error

--- FileConvert/FileConvert/Properties/AssemblyInfo.cs
+++ FileConvert/FileConvert/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: publicsVisibleTo("FileConvert.Tests")]
-[assembly: publicsVisibleTo("FileConvert.Explorables")]
+[assembly: InternalsVisibleTo("FileConvert.Tests")]
+[assembly: InternalsVisibleTo("FileConvert.Explorables")]
 
